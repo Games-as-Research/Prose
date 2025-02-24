9044a864a499f561ed37affc97ea6dcb9646239a
Shahrom <mshahromali@gmail.com>
2025-02-24 01:37:44 -0500

    Prototype Zero Complete
    Prototype Zero is meant to be a direct translation of the article from a PDF reader into a web app. This is meant to demonstrate the article as fluid. Now that the web app affords fluidity and the article takes the shape of its container we can start visualizing what more can be done to this.
    
    Only thing missing now are images

--- src/zero/prototypeContext.jsx
+++ src/zero/prototypeContext.jsx
@@ -1,34 +1,13 @@
 import { createContext, useState } from "react";
-import ArticleData from "./ArticleData";
+import ArticleData from "../assets/ArticleData";
 
 const PrototypeContext = createContext();
 
 export const PrototypeProvider = (props) => {
-  const [showAbstract, setShowAbstract] = useState(false);
-  const [section, setSection] = useState(0);
-
-  function NextSection() {
-    setSection((section + 1) % ArticleData.sections.length);
-  }
-
-  function PreviousSection() {
-    if (section === 0) {
-      setSection(ArticleData.sections.length - 1);
-    } else {
-      setSection(section - 1);
-    }
-  }
-
   return (
     <PrototypeContext.Provider
       value={{
         ArticleData,
-
-        showAbstract,
-        section,
-        setShowAbstract,
-        NextSection,
-        PreviousSection,
       }}
     >
       {props.children}
--- src/zero/zero.jsx
+++ src/zero/zero.jsx
@@ -2,11 +2,12 @@ import Markdown from "react-markdown";
 import { ScreenContainer } from "../common";
 import PrototypeContext, { PrototypeProvider } from "./prototypeContext";
 import { useContext, useState } from "react";
+import remarkGfm from "remark-gfm";
 
 const Zero = (props) => {
   return (
     <PrototypeProvider>
-      <ScreenContainer>
+      <ScreenContainer className="flex-col">
         <ControlPanel />
         <Article />
       </ScreenContainer>
@@ -25,64 +26,49 @@ const Article = (props) => {
         title={PC.ArticleData.title}
         abstract={PC.ArticleData.abstract}
         keywords={PC.ArticleData.keywords}
+        year={PC.ArticleData.year}
+        authors={PC.ArticleData.authors.join(", ")}
+        citation={PC.ArticleData.citation}
       />
 
-      <div className="flex overflow-y-hidden">
-        <ArticleSection
-          sectionTitle={
-            (PC.section + 1).toString() +
-            ". " +
-            PC.ArticleData.sections[PC.section].title
-          }
-          content={PC.ArticleData.sections[PC.section].paragraphs}
-        />
+      <div className="flex flex-col">
+        {PC.ArticleData.sections.map((itm, itx) => {
+          return (
+            <ArticleSection
+              sectionTitle={(itx + 1).toString() + ". " + itm.title}
+              content={itm.paragraphs}
+            />
+          );
+        })}
       </div>
+      <TheBibliography />
     </div>
   );
 };
 
 const ArticleAbstract = (props) => {
-  const [hold, setHold] = useState(false);
-  const PC = useContext(PrototypeContext);
-
-  if (!PC.showAbstract) {
-    return (
-      <h1
-        className="text-center text-md my-2 text-wrap mx-1 font-bold py-4"
-        onMouseOver={() => (hold ? null : PC.setShowAbstract(true))}
-        onClick={() => setHold(!hold)}
-      >
-        {props.title}
-      </h1>
-    );
-  }
-
   return (
-    <div
-      className="bg-slate-200 rounded-md py-4"
-      onMouseLeave={() => {
-        if (!hold) {
-          PC.setShowAbstract(false);
-          setHold(false);
-        }
-      }}
-      onClick={() => setHold(!hold)}
-    >
+    <div className="bg-slate-200 rounded-md py-4">
       <h1 className="text-center text-md my-2 text-wrap mx-1 font-bold">
         {props.title}
       </h1>
-      <h2
-        className="text-center text-md my-5 mx-4 font-semibold"
-        style={{ color: hold ? "blue" : "black" }}
-      >
-        Abstract
-      </h2>
-      <Markdown className={"text-justify text-wrap text-md my-2 mx-5"}>
+      <p className="text-center text-sm my-2 text-wrap mx-1">{props.authors}</p>
+      <p className="text-center text-sm my-2 text-wrap mx-1">
+        {"(" + props.year + ")"}
+      </p>
+      <h2 className="text-center text-md my-5 mx-4 font-semibold">Abstract</h2>
+      <Markdown className={"text-left text-wrap text-md my-2 mx-5"}>
         {props.abstract}
       </Markdown>
       <div className="flex mx-5">
-        <p className="font-bold mr-1">{"Keywords: "}</p>
+        <p className="font-bold mr-2">{"Keywords: "}</p>
         <p className="text-md">{props.keywords.join("; ")}</p>
+      </div>
+      <div className="flex mx-5 mt-2">
+        <p className="font-bold mr-2">{"Reference: "}</p>
+        <Markdown className={"text-sm text-blue-800"}>
+          {props.citation}
+        </Markdown>
       </div>
     </div>
   );
@@ -92,16 +78,10 @@ const ArticleSection = (props) => {
 
   return (
     <div className="flex flex-col bg-slate-50 rounded-md overflow-y-scroll">
-      <div className="flex flex-row justify-between my-5 mx-4">
-        <button className="font-bold" onClick={PC.PreviousSection}>
-          {"<"}
-        </button>
+      <div className="flex flex-row justify-center my-5 mx-4">
         <h2 className="text-center text-md font-semibold ">
           {props.sectionTitle}
         </h2>
-        <button className="font-bold" onClick={PC.NextSection}>
-          {">"}
-        </button>
       </div>
 
       <div className="flex flex-col overflow-y-scroll bg-slate-100 rounded-md">
@@ -109,7 +89,8 @@ const ArticleSection = (props) => {
           return (
             <Markdown
               key={idx}
-              className={"text-justify text-wrap text-md my-2 mx-5"}
+              remarkPlugins={[remarkGfm]}
+              className={"text-wrap text-md my-2 mx-5"}
             >
               {item}
             </Markdown>
@@ -120,33 +101,51 @@ const ArticleSection = (props) => {
   );
 };
 
+const TheBibliography = (props) => {
+  const PC = useContext(PrototypeContext);
+
+  return (
+    <div className="flex flex-col bg-slate-50 rounded-md overflow-y-scroll">
+      <div className="flex flex-row justify-center my-5 mx-4">
+        <h2 className="text-center text-md font-semibold ">Bibliography</h2>
+      </div>
+      <div className="flex flex-col overflow-y-scroll bg-slate-100 rounded-md">
+        {PC.ArticleData?.bibliography.map((item, idx) => {
+          return (
+            <Markdown key={idx} className={"text-wrap text-md my-2 mx-5"}>
+              {"[" + item.id + "] " + item.title}
+            </Markdown>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 const ControlPanel = (props) => {
   return (
-    <div className="bg-black text-white w-2/5 h-full flex-col font-mono text-center">
+    <div className="flex  w-screen bg-black text-white  font-mono p-4 justify-between">
       {/* Title and Intro */}
 
-      <div className="">
-        <h1 className="text-white  font-medium text-xl my-5">
-          Prose: Prototype Zero
-        </h1>
-        <p className="text-white text-wrap font-light text-sm mx-5">
-          This is the base prototype that deals with the core structure of a
-          scientific article.
-        </p>
-      </div>
+      <div className="flex flex-row justify-between w-full">
+        <h1 className="text-white font-medium text-xl">Prototype Zero</h1>
 
-      {/* Controls */}
-      <div className="flex flex-col my-10">
-        <div>
-          <h2>Font Size</h2>
-        </div>
+        {/* Controls */}
+        <div className="flex flex-row self-center">
+          <p className="text-xs">
+            Affording fluidity and erasing the article-reader boundary.
+          </p>
+          {/* <div>
+            <h2>Font Size</h2>
+          </div>
 
-        <div>
-          <h2>Font Family</h2>
-        </div>
+          <div>
+            <h2>Font Family</h2>
+          </div>
 
-        <div>
-          <h2>Font Alignment</h2>
+          <div>
+            <h2>Font Alignment</h2>
+          </div> */}
         </div>
       </div>
     </div>
