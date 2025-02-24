bad30ec56fedc6e0fb6e00e04fda5227e1b7c113
Shahrom <mshahromali@gmail.com>
2025-02-24 01:19:57 -0500

    ability to override styling with props

--- src/common.jsx
+++ src/common.jsx
@@ -2,7 +2,11 @@ export const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export const ScreenContainer = (props) => {
   return (
-    <div className="flex flex-row h-screen w-screen max-md:flex-col">
+    <div
+      className={
+        "flex flex-row h-screen w-screen max-md:flex-col " + props.className
+      }
+    >
       {props.children}
     </div>
   );
