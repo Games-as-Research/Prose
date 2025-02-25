5e0cd6c1aa74b91219804fea015c5d253a339b38
Shahrom <mshahromali@gmail.com>
2025-02-25 10:50:36 -0500

    Prototype Zero: Feedback
    Feedback from other prototypes, I wanted to explore how this version could grow. Like I said last night, there *is* a charm to the simple long-scroll but its not as feature rich as the others. So what if we enhance this too?
    
    I have added the margins, hover, bold, font family, and paragraph numbers. How does this change my reading in this version?

--- src/zero/prototypeContext.jsx
+++ src/zero/prototypeContext.jsx
@@ -4,10 +4,53 @@ import ArticleData from "../assets/ArticleData";
 const PrototypeContext = createContext();
 
 export const PrototypeProvider = (props) => {
+  const [horizontalMargins, setHorizontalMargins] = useState(4);
+  const [version, setVersion] = useState(0.0);
+  const [fontFam, setFontFam] = useState("font-sans");
+  const [bold, setBold] = useState(false);
+  const [hover, setHover] = useState(false);
+
+  function ChangeVersion() {
+    if (version === 0.0) {
+      setVersion(0.1);
+    } else {
+      setVersion(0.0);
+    }
+  }
+  function SwitchFont() {
+    if (fontFam == "font-serif") {
+      setFontFam("font-sans");
+    } else if (fontFam == "font-sans") {
+      setFontFam("font-mono");
+    } else if (fontFam == "font-mono") {
+      setFontFam("font-serif");
+    }
+  }
+
+  function ToggleBold() {
+    if (bold) setBold(false);
+    else setBold(true);
+  }
+
+  function ToggleHover() {
+    if (hover) setHover(false);
+    else setHover(true);
+  }
+
   return (
     <PrototypeContext.Provider
       value={{
+        version,
+        hover,
+        bold,
+        fontFam,
         ArticleData,
+        horizontalMargins,
+        setHorizontalMargins,
+        ChangeVersion,
+        ToggleBold,
+        SwitchFont,
+        ToggleHover,
       }}
     >
       {props.children}
--- src/zero/zero.jsx
+++ src/zero/zero.jsx
@@ -1,8 +1,9 @@
 import Markdown from "react-markdown";
 import { ScreenContainer } from "../common";
 import PrototypeContext, { PrototypeProvider } from "./prototypeContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import remarkGfm from "remark-gfm";
+import { Slider } from "antd";
 
 const Zero = (props) => {
   return (
@@ -21,7 +22,7 @@ const Article = (props) => {
   const PC = useContext(PrototypeContext);
 
   return (
-    <div className="flex flex-col bg-slate-50 w-full font-sans">
+    <div className={"flex flex-col bg-slate-50 w-full " + PC.fontFam}>
       <ArticleAbstract
         title={PC.ArticleData.title}
         abstract={PC.ArticleData.abstract}
@@ -47,8 +48,15 @@ const Article = (props) => {
 };
 
 const ArticleAbstract = (props) => {
+  const PC = useContext(PrototypeContext);
   return (
-    <div className="bg-slate-200 rounded-md py-4">
+    <div
+      className="bg-slate-200 rounded-md py-4"
+      style={{
+        marginLeft: PC.horizontalMargins,
+        marginRight: PC.horizontalMargins,
+      }}
+    >
       <h1 className="text-center text-md my-2 text-wrap mx-1 font-bold">
         {props.title}
       </h1>
@@ -75,22 +83,79 @@ const ArticleAbstract = (props) => {
 };
 const ArticleSection = (props) => {
   const PC = useContext(PrototypeContext);
+  const base_style = "text-wrap text-md rounded-lg  my-2 mx-5 ";
+  const hover_style =
+    " hover:-translate-y-1 hover:bg-slate-200 hover:shadow-md transition delay-50 duration-300 ease-in-out ";
+  const bold_style =
+    " hover:font-semibold transition delay-50 duration-300 ease-in-out ";
+
+  if (PC.version === 0.1) {
+    let para_count = 0;
+    return (
+      <div className="flex flex-col bg-slate-50 rounded-md">
+        <div className="flex flex-row justify-center my-5 mx-4">
+          <h2 className="text-center text-md font-semibold ">
+            {props.sectionTitle}
+          </h2>
+        </div>
+        <div className="flex flex-col rounded-md">
+          {props.content?.map((item, idx) => {
+            return (
+              <div
+                className={"flex flex-row py-2"}
+                key={idx}
+                style={{
+                  marginLeft: PC.horizontalMargins,
+                  marginRight: PC.horizontalMargins,
+                }}
+              >
+                {item?.slice(0, 2) !== "##" ? (
+                  <p className="self-center text-xs font-light text-slate-400 font-sans ml-2">
+                    {++para_count}
+                  </p>
+                ) : null}
+                <Markdown
+                  className={
+                    base_style +
+                    (PC.hover ? hover_style : "") +
+                    (PC.bold ? bold_style : "")
+                  }
+                >
+                  {item}
+                </Markdown>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className="flex flex-col bg-slate-50 rounded-md overflow-y-scroll">
+    <div className="flex flex-col bg-slate-50 rounded-md">
       <div className="flex flex-row justify-center my-5 mx-4">
         <h2 className="text-center text-md font-semibold ">
           {props.sectionTitle}
         </h2>
       </div>
 
-      <div className="flex flex-col overflow-y-scroll bg-slate-100 rounded-md">
+      <div
+        className="flex flex-col bg-slate-100 rounded-md"
+        style={{
+          marginLeft: PC.horizontalMargins,
+          marginRight: PC.horizontalMargins,
+        }}
+      >
         {props.content?.map((item, idx) => {
           return (
             <Markdown
               key={idx}
               remarkPlugins={[remarkGfm]}
-              className={"text-wrap text-md my-2 mx-5"}
+              className={
+                base_style +
+                (PC.hover ? hover_style : "") +
+                (PC.bold ? bold_style : "")
+              }
             >
               {item}
             </Markdown>
@@ -103,9 +168,40 @@ const ArticleSection = (props) => {
 
 const TheBibliography = (props) => {
   const PC = useContext(PrototypeContext);
-
+  if (PC.version === 0.1) {
+    return (
+      <div
+        className="flex flex-col bg-slate-50 rounded-md"
+        style={{
+          marginLeft: PC.horizontalMargins,
+          marginRight: PC.horizontalMargins,
+        }}
+      >
+        <div className="flex flex-col  bg-slate-100 rounded-md">
+          {PC.ArticleData?.bibliography.map((item, idx) => {
+            return (
+              <div key={idx} className={"flex flex-row py-2"}>
+                <p className="self-center text-xs font-light text-slate-400 font-sans ml-8 mr-4">
+                  {"[" + item.id + "] "}
+                </p>
+                <Markdown className="text-wrap text-sm rounded-lg hover:font-semibold hover:-translate-y-1 hover:text-sm hover:bg-slate-200 hover:shadow-md hover:p-1 transition delay-50 duration-300 ease-in-out">
+                  {item.title}
+                </Markdown>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="flex flex-col bg-slate-50 rounded-md overflow-y-scroll">
+    <div
+      className="flex flex-col bg-slate-50 rounded-md overflow-y-scroll "
+      style={{
+        marginLeft: PC.horizontalMargins,
+        marginRight: PC.horizontalMargins,
+      }}
+    >
       <div className="flex flex-row justify-center my-5 mx-4">
         <h2 className="text-center text-md font-semibold ">Bibliography</h2>
       </div>
@@ -123,6 +219,7 @@ const TheBibliography = (props) => {
 };
 
 const ControlPanel = (props) => {
+  const PC = useContext(PrototypeContext);
   return (
     <div className="flex  w-screen bg-black text-white  font-mono p-4 justify-between">
       {/* Title and Intro */}
@@ -131,21 +228,58 @@ const ControlPanel = (props) => {
         <h1 className="text-white font-medium text-xl">Prototype Zero</h1>
 
         {/* Controls */}
-        <div className="flex flex-row self-center">
-          <p className="text-xs">
-            Affording fluidity and erasing the article-reader boundary.
-          </p>
-          {/* <div>
-            <h2>Font Size</h2>
-          </div>
+        <div className="flex flex-row self-center w-[70%] space-x-5 justify-end">
+          {PC.version == 0.1 ? (
+            <div className="bg-white w-[20%] rounded-md px-2">
+              <Slider
+                min={0}
+                step={1}
+                max={75}
+                onChange={(val) => {
+                  PC.setHorizontalMargins(val * 4);
+                }}
+                value={
+                  typeof PC.horizontalMargins === "number"
+                    ? PC.horizontalMargins / 4
+                    : 1
+                }
+              />
+            </div>
+          ) : null}
 
-          <div>
-            <h2>Font Family</h2>
-          </div>
+          {PC.version == 0.1 ? (
+            <button
+              className="bg-white text-black text-sm p-1 rounded-md hover:bg-slate-400"
+              onClick={PC.SwitchFont}
+            >
+              {PC.fontFam.replace("-", " ")}
+            </button>
+          ) : null}
 
-          <div>
-            <h2>Font Alignment</h2>
-          </div> */}
+          {PC.version == 0.1 ? (
+            <button
+              className="bg-white text-black text-sm p-1 rounded-md hover:bg-slate-400"
+              onClick={PC.ToggleBold}
+            >
+              {PC.bold ? "bold on hover" : "no bold"}
+            </button>
+          ) : null}
+
+          {PC.version == 0.1 ? (
+            <button
+              className="bg-white text-black text-sm p-1 rounded-md hover:bg-slate-400"
+              onClick={PC.ToggleHover}
+            >
+              {PC.hover ? "foregrounding" : "no foregrounding"}
+            </button>
+          ) : null}
+
+          <button
+            className="bg-white text-black text-sm p-1 rounded-md hover:bg-slate-400"
+            onClick={PC.ChangeVersion}
+          >
+            Version {PC.version}
+          </button>
         </div>
       </div>
     </div>
